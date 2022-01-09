6df89fcc6ce87d418b3e84fd671fe3e9f405807e
torvista <4391026+torvista@users.noreply.github.com>
2022-01-09 19:57:45 +0100

    initial commit

---
+++ ADMIN_FOLDER/zz_lang_creator.php
@@ -0,0 +1,207 @@
+<?php
+/** @link https://github.com/torvista/Zen_Cart-Language_File_Converter
+ * @version $Id: 09/01/2022 torvista
+ */
+
+$language_to_convert = 'spanish'; //set to your language fileset name
+
+$convert_admin_files = false; // set to true to create admin files
+
+$convert_shopfront_files = false; // set to true to create shopfront files
+
+$allow_create_files = false; // prevent any file creation or WILL OVERWRITE THE NEW FILES EACH TIME THIS FILE IS RUN!!!
+
+$debug = false; //set to true to display processing info
+
+/////////////////////////////////////////////
+require('includes/application_top.php');
+$paths_to_scan = [];
+if ($convert_admin_files) {
+    $paths_to_scan = [
+        DIR_FS_ADMIN . DIR_WS_LANGUAGES,
+        DIR_FS_ADMIN . DIR_WS_LANGUAGES . $language_to_convert . '/',
+        DIR_FS_ADMIN . DIR_WS_LANGUAGES . $language_to_convert . '/extra_definitions/',
+        DIR_FS_ADMIN . DIR_WS_LANGUAGES . $language_to_convert . '/modules/newsletters/',
+    ];
+}
+if ($convert_shopfront_files) {
+    $paths_to_scan = [
+        DIR_FS_CATALOG_LANGUAGES,
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/classic/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/extra_definitions/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/extra_definitions/classic/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/extra_definitions/responsive_classic/',
+       // DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/html_includes/',
+        //DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/html_includes/classic/',
+       // DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/html_includes/responsive_classic/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/modules/order_total/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/modules/order_total/classic/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/modules/order_total/responsive_classic/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/modules/payment/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/modules/payment/classic/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/modules/payment/responsive_classic/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/modules/shipping/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/modules/shipping/classic/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/modules/shipping/responsive_classic/',
+        DIR_FS_CATALOG_LANGUAGES . $language_to_convert . '/responsive_classic/',
+    ];
+}
+if (!function_exists('mv_printVar')) {//debugging tool only
+    /**
+     * @param $a
+     */
+    function mv_printVar($a)
+    {
+        $backtrace = debug_backtrace()[0];
+        $fh = fopen($backtrace['file'], 'rb');
+        $line = 0;
+        $code = '';
+        while (++$line <= $backtrace['line']) {
+            $code = fgets($fh);
+        }
+        fclose($fh);
+        preg_match('/' . __FUNCTION__ . '\s*\((.*)\)\s*;/u', $code, $name);
+        echo '<pre>';
+        if (!empty($name[1])) {
+            echo '<strong>' . trim($name[1]) . '</strong> (' . gettype($a) . "):\n";
+        }
+        //var_export($a);
+        print_r($a);
+        echo '</pre><br>';
+    }
+}
+?>
+    <!doctype html>
+    <html <?php echo HTML_PARAMS; ?>>
+    <head>
+        <?php require DIR_WS_INCLUDES . 'admin_html_head.php'; ?>
+    </head>
+    <body>
+    <!-- header //-->
+
+    <?php require DIR_WS_INCLUDES . 'header.php'; ?>
+    <!-- header_eof //-->
+
+    <!-- body //-->
+    <div class="container-fluid">
+        <!-- body_text //-->
+        <h1>Create lang.*.php files for : "<?php echo $language_to_convert; ?>"</h1>
+        <?php
+            if ($convert_admin_files) {
+             echo '<h2>create lang.*.php files for admin</h2>';
+                }
+            if ($convert_shopfront_files) {
+             echo '<h2>create lang.*.php files for shopfront</h2>';
+                }
+        if (!$convert_admin_files && !$convert_shopfront_files) {
+             echo '<h2>no files set for conversion</h2>';
+        }
+        if ($debug) {
+            mv_printVar($paths_to_scan);
+        }
+        foreach ($paths_to_scan as $path_to_scan) {
+            echo '<h2>path to scan:' . $path_to_scan . '</h2>';
+
+            //get the main language file
+            if (file_exists($path_to_scan . $language_to_convert . ".php")) {
+                $filelist = [$path_to_scan . $language_to_convert . ".php"];
+            } else {
+                $filelist = glob($path_to_scan . "*.php");
+            }
+            if ($debug) {
+                echo '$filelist: all files';
+                mv_printVar($filelist);
+            }
+
+            //filter out any pre-existing lang.*.php files
+            $filelist = preg_grep("/lang\./", $filelist, PREG_GREP_INVERT);
+            if ($debug) {
+                echo '$filelist: any pre-existing lang.*.php removed';
+                mv_printVar($filelist);
+            }
+
+            foreach ($filelist as $filename) {
+                echo '<p>File to convert: <b>' . $filename . '</b></p>';
+
+                $contents = trim(file_get_contents($filename));
+
+                //remove comments
+                $contents = preg_replace('!/\*.*?\*/!s', '', $contents);
+                // echo $contents;die;
+                foreach (token_get_all($contents) as $token) {
+                    if ($token[0] !== T_COMMENT) {
+                        continue;
+                    }
+                    $contents = str_replace($token[1], '', $contents);
+                }
+
+                //remove empty lines
+                $contents = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $contents);
+
+                //remove php end tag
+                $contents_length = strlen($contents);
+                $pos_end_tag = strrpos($contents, "?>");
+                if ($pos_end_tag === $contents_length - 2) {
+                    if ($debug) {
+                        echo '<p>php end tag "?>" found and removed<p>';
+                    }
+                    $contents = substr_replace($contents, '', $pos_end_tag, 2);
+                }
+
+                //make leading "define" structure consistent
+                $contents = str_replace("define ('", "define('", $contents);
+                $contents = str_replace("define ( '", "define('", $contents);
+
+                //make definition comma separator consistent (no space)
+                $contents = str_replace("' ,'", "','", $contents);
+                $contents = str_replace("' , '", "','", $contents);
+                $contents = str_replace("', '", "','", $contents);
+                $contents = str_replace("',  '", "','", $contents);
+
+                //remove "define"
+                $contents = str_replace("define('", "    '", $contents);
+
+                //replace middle comma with  =>
+                $contents = str_replace("','", "' => '", $contents);
+
+                //remove trailing );
+                $contents = str_replace(");", ",", $contents);
+
+                //find start of array list
+                $start = strpos($contents, "    '");
+                if ($start === false) {
+                    echo '<p class="messageStackWarning">Error: start of constants not identified.</p>';
+                }
+                $contents = substr_replace($contents, '$define = [' . "\n", $start, 0);
+
+                //end of array list
+                $contents .= "\n" . '];';
+                $contents .= "\n\n" . 'return $define;';
+
+                $file_info = pathinfo($filename);
+                $filename_lang = $file_info['dirname'] . '/lang.' . $file_info['filename'] . '.php';
+
+                if ($allow_create_files) {
+                     file_put_contents($filename_lang, $contents);
+                    echo '<p>New file created:<b>' . $filename_lang . '</b></p>';
+                    echo '<hr style="text-align:left;width:75%;margin-right:100%">';
+                } else {
+                    echo '<p>New file would be created (currently disabled in script):<b>' . $filename_lang . '</b></p>';
+                }
+            }
+            echo '<hr>';
+        }
+        ?>
+        <!-- body_text_eof //-->
+    </div>
+    <!-- body_eof //-->
+
+    <!-- footer //-->
+    <?php require DIR_WS_INCLUDES . "footer.php"; ?>
+    <!-- footer_eof //-->
+
+    </body>
+    </html>
+<?php
+require DIR_WS_INCLUDES . 'application_bottom.php';
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# Zen_Cart-Language_File_Converter
+# Zen Cart - Language File Converter
 Utility to convert pre ZC158 language files to array format.
+
+This is imperfect as there are too many oddities in the original files to do a 100% automatic conversion.
+This will create lang copies of the files in the correct places to enable comparison with the originals for manual tweaking.
+
+##Useage
+1) DO NOT USE ON A LIVE SITE.
+2) TRY THIS ON A COPY OF YOUR LIVE SITE
+3) DO NOT USE ON A LIVE SITE.
+
+IS THAT CLEAR ENOUGH?
+
+1) Copy the file into your admin directory: from here it can access the shopfront files too.
+2) Open the file to edit it.
+
+There are some options at the start of the file. Edit as required. I suggest you do the shopfront first.
+
+$language_to_convert = 'spanish'; //set to your language fileset
+$convert_admin_files = true; // set to true to create admin files
+$convert_shopfront_files = false; // set to true to create shopfront files
+
+$allow_create_files = false; // prevent any file creation or WILL OVERWRITE THE NEW FILES EACH TIME THIS FILE IS RUN!!!
+
+$debug = true; //set to true to display processing info
+
+2) Log into your admin
+3) Manually type the filename: YOUR_ADMIN/zz_lang_creator.php
+If you have set to $allow_create_files to true, new files prefixed with .lang will be created in the same places.
+
+Refreshing the shopfront will most likely result in a white screen of death and a debug log file which will indicate the location of the error.
+There will be multiple errror, for various reasons. Just work your way through them.
+
+IF YOU RUN THE SCRIPT AGAIN IT WILL OVERWRITE YOUR CHANGES SO IMMEDIATELY RESET THE OPTIONS TO PREVENT THIS.
+
+Use the information in the debug logs to correct the errors by comparing the file with the original.
+There will be a comment fragments breaking the format and also embedded constants which need to be escaped with doubled percentage markers
+
+For example
+define('BOX_GV_ADMIN_QUEUE', 'Listado de ' . TEXT_GV_NAMES);
+
+becomes
+   'BOX_GV_ADMIN_QUEUE' => 'Listado de ' . '%%TEXT_GV_NAMES%%',
+
+
+##How it works
+The script will parse the hard-coded paths, retrieve the list of the files therein and create new ones prefixed with .lang in the same places.
+It just uses basic search and replacing which was intended to be quick to code, but ended up not so quick to do but to continue trying to make it perfect is subject to the law of diminishing returns, but go ahead and try.
+I will NOT be making more changes to it.
+
+
+
+
